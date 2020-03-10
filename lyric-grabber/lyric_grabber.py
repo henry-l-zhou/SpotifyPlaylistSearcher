4c00af9aac3c66a0f7ce914f8342fc84dc2af9e3
Brian Hu <bhurobin221@gmail.com>
2020-03-10 13:03:27 -0500

    added multiprocessing

--- lyric-grabber/lyric_grabber.py
+++ lyric-grabber/lyric_grabber.py
@@ -3,6 +3,8 @@ import os
 import requests
 import re
 from bs4 import BeautifulSoup
+import multiprocessing as mp
+import numpy as np
 
 agent = 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) \
         Gecko/20100101 Firefox/24.0'
@@ -11,12 +13,13 @@ headers = {'User-Agent': agent}
 if(os.path.exists("output.csv")):
     os.remove("output.csv")
 
-df = pd.read_csv("file.csv")
+df = pd.read_csv("../SpotifyAPI/ArtistAndSongs.csv")
 
 def lyrics(artist, title):
     base = "https://azlyrics.com/"
     artistURI = re.sub('[\W_]+', '', artist.lower())
-    titleURI = re.sub('[\W_]+', '', title.lower())
+    titleURI = title.split('-', 1)[0].split('(', 1)[0]
+    titleURI = re.sub('[\W_]+', '', titleURI.lower())
 
     url = base + 'lyrics/' + artistURI + '/' + titleURI + '.html'
     req = requests.get(url, headers=headers)
@@ -26,12 +29,26 @@ def lyrics(artist, title):
         return "Unable to find " + title + " by " + artist
     else:
         l = [x.getText() for x in l]
-        l = re.sub('', '', l)
         return l
 
 def grabLyrics(row):
+    print(row)
     return lyrics(row[1],row[0])
-lyrics = df.apply(grabLyrics, axis=1)
+
+def applyGrabLyrics(df):
+    lyrics = df.apply(grabLyrics,axis=1)
+    return lyrics
+
+n_proc = mp.cpu_count()
+p = mp.Pool(n_proc)
+split_dfs = np.array_split(df, n_proc)
+pool_res = p.map(applyGrabLyrics, split_dfs)
+p.close()
+p.join()
+
+print(pool_res)
+
+lyrics = pd.concat(pool_res, axis=0)
 
 df["Lyrics"] = lyrics
 
