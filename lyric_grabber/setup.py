2cc36d452a95f37c695af391eb563961e187d5d4
Brian Hu <bhurobin221@gmail.com>
2020-03-09 22:36:27 -0500

    add setup.py

--- lyric_grabber/setup.py
+++ lyric_grabber/setup.py
@@ -1,3 +1,16 @@
 from setuptools import setup, find_packages
 
-
+setup(
+    name='lyric-grabber',
+    packages=find_packages(),
+    url='https://github.com/henry-l-zhou/SpotifyPlaylistSearcher/lyric-grabber',
+    description='Grabs lyrics from a list of songs',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "requests==2.7.0",
+        ],
+    dependency_links = [
+     "git+git://github.com/elmoiv/azapi",
+    ],
+    include_package_data=True,
+)
