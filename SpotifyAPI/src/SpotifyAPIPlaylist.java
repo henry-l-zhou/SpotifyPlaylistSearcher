fb9c929edbb300d22617620e0bc6ad2066b534cc
Henry Zhou <42553559+henry-l-zhou@users.noreply.github.com>
2020-03-10 00:40:27 -0500

    Hey Brian I'm cooler than you and using Source Tree.

---
+++ SpotifyAPI/src/SpotifyAPIPlaylist.java
@@ -0,0 +1,61 @@
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.specification.Paging;
+import com.wrapper.spotify.model_objects.specification.PlaylistTrack;
+import com.wrapper.spotify.requests.data.playlists.GetPlaylistsTracksRequest;
+import java.io.IOException;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+
+public class SpotifyAPIPlaylist extends SpotifyAPI {
+//  private static final String accessToken =
+//      "BQDAtQzJc0gJS-qYuNdlMEU9SDGFlr0N3voJYsw5rOuXBas85_Ctciifx1X0smRd0m0xBSrlzfOupo9IxYM";
+  private static final String playlistId = "6hCtSk1sPDFctoc9cvuWqO";
+
+  public SpotifyAPIPlaylist() {
+    
+  }
+
+  private static final SpotifyApi spotifyApi =
+      new SpotifyApi.Builder().setAccessToken(SpotifyAPI.clientCredentials_Sync()).build();
+  private static final GetPlaylistsTracksRequest getPlaylistsTracksRequest =
+      spotifyApi.getPlaylistsTracks(playlistId)
+          //          .fields("description")
+          //          .limit(10)
+          //          .offset(0)
+          //          .market(CountryCode.SE)
+          .build();
+
+  public static void getPlaylistsTracks_Sync() {
+    try {
+      final Paging<PlaylistTrack> playlistTrackPaging = getPlaylistsTracksRequest.execute();
+
+      System.out.println("Total: " + playlistTrackPaging.getTotal());
+    } catch (IOException | SpotifyWebApiException e) {
+      System.out.println("Error: " + e.getMessage());
+    }
+  }
+
+  public static void getPlaylistsTracks_Async() {
+    try {
+      final CompletableFuture<Paging<PlaylistTrack>> pagingFuture =
+          getPlaylistsTracksRequest.executeAsync();
+
+      // Thread free to do other tasks...
+
+      // Example Only. Never block in production code.
+      final Paging<PlaylistTrack> playlistTrackPaging = pagingFuture.join();
+
+      System.out.println("Total: " + playlistTrackPaging.getTotal());
+    } catch (CompletionException e) {
+      System.out.println("Error: " + e.getCause().getMessage());
+    } catch (CancellationException e) {
+      System.out.println("Async operation cancelled.");
+    }
+  }
+
+  public static void main(String[] args) {
+    getPlaylistsTracks_Sync();
+  }
+}
