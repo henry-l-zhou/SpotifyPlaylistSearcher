0acd7f996af619fcfe2c00be1f0430d0843ad146
Henry Zhou <42553559+henry-l-zhou@users.noreply.github.com>
2020-03-10 10:34:28 -0500

    fixed weird commenting

--- SpotifyAPI/src/SpotifyAPIPlaylist.java
+++ SpotifyAPI/src/SpotifyAPIPlaylist.java
@@ -1,7 +1,5 @@
 import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
-import com.wrapper.spotify.model_objects.specification.Artist;
-import com.wrapper.spotify.model_objects.specification.ArtistSimplified;
 import com.wrapper.spotify.model_objects.specification.Paging;
 import com.wrapper.spotify.model_objects.specification.PlaylistTrack;
 import com.wrapper.spotify.requests.data.playlists.GetPlaylistsTracksRequest;
@@ -11,22 +9,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SpotifyAPIPlaylist extends SpotifyAPI {
-<<<<<<< HEAD
-  //  private static final String accessToken =
-  //      "BQDAtQzJc0gJS-qYuNdlMEU9SDGFlr0N3voJYsw5rOuXBas85_Ctciifx1X0smRd0m0xBSrlzfOupo9IxYM";
-  private static final String playlistId = "6hCtSk1sPDFctoc9cvuWqO";
-
-  public SpotifyAPIPlaylist() {
-
-  }
-
-=======
   private static final String playlistId = "6hCtSk1sPDFctoc9cvuWqO";
 
   private static List<ArtistSong> tracks = new ArrayList<ArtistSong>();
   private static int totalSongs;
   private static int songsAdded;
->>>>>>> 966bd8cfc5c7d74e2e42463448cd4df36613345b
   private static final SpotifyApi spotifyApi =
       new SpotifyApi.Builder().setAccessToken(SpotifyAPI.clientCredentials_Sync()).build();
 
@@ -44,12 +31,7 @@ public class SpotifyAPIPlaylist extends SpotifyAPI {
   private static GetPlaylistsTracksRequest getPlaylistsTracksRequest =
       spotifyApi.getPlaylistsTracks(playlistId)
           //          .fields("description")
-<<<<<<< HEAD
-          .limit(100)
-          .offset(0)
-=======
           .limit(100).offset(0)
->>>>>>> 966bd8cfc5c7d74e2e42463448cd4df36613345b
           //          .market(CountryCode.SE)
           .build();
 
@@ -57,14 +39,6 @@ public class SpotifyAPIPlaylist extends SpotifyAPI {
     try {
       final Paging<PlaylistTrack> playlistTrackPaging = getPlaylistsTracksRequest.execute();
 
-<<<<<<< HEAD
-      System.out.println("Total: " + playlistTrackPaging.getTotal());
-      for (PlaylistTrack playlistTrack : playlistTrackPaging.getItems()) {
-        System.out.print(playlistTrack.getTrack().getName() + " ||| ");
-        System.out.print(playlistTrack.getTrack().getArtists()[0].getName());
-
-        System.out.println();
-=======
       totalSongs = playlistTrackPaging.getTotal();
       for (PlaylistTrack playlistTrack : playlistTrackPaging.getItems()) {
         //        System.out.print(playlistTrack.getTrack().getName() + " ||| ");
@@ -72,7 +46,6 @@ public class SpotifyAPIPlaylist extends SpotifyAPI {
         songsAdded++;
         tracks.add(new ArtistSong(playlistTrack.getTrack().getArtists()[0].getName(),
             playlistTrack.getTrack().getName()));
->>>>>>> 966bd8cfc5c7d74e2e42463448cd4df36613345b
       }
     } catch (IOException | SpotifyWebApiException e) {
       System.out.println("Error: " + e.getMessage());
