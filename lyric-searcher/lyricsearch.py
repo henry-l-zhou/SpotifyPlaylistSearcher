fde97c10846ef1622dc04c4e03a7b23a5336ef14
Henry Zhou <42553559+henry-l-zhou@users.noreply.github.com>
2020-03-11 15:47:52 -0500

    basic search feature implemented

---
+++ lyric-searcher/lyricsearch.py
@@ -0,0 +1,32 @@
+import csv
+import re
+import pandas as pd
+
+# with open('../lyric-grabber/output.csv', newline='', encoding="utf8") as csvfile:
+#     spamreader = csv.reader(csvfile)
+#     for row in spamreader:
+#     	if "love" in row[2]:
+#     		print (row)
+#     		print ("\n")
+
+user_input = "been"
+
+df = pd.read_csv("../lyric-grabber/output.csv")
+searched_list = []
+for i in range(df.shape[0]):
+	count = 0;
+
+	words = df.iloc[i]["Lyrics"]
+
+	words = re.findall(r"[\w']+|[.,!?;]", words)
+
+	
+	for word in words:
+
+		if user_input in word:
+			count += 1
+
+	searched_list.append([df.iloc[i]["title"], df.iloc[i]["artist"], count])
+
+searched_list.sort(key = lambda r:r[2], reverse = True)
+print (searched_list)
