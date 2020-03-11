1dabfc7f816c9ff61aca9c19db1f422f1fabba60
Brian Hu <bhurobin221@gmail.com>
2020-03-10 23:47:41 -0500

    added more processors to lyric grabber, switched to genius API

--- lyric-grabber/lyric_grabber.py
+++ lyric-grabber/lyric_grabber.py
@@ -1,48 +1,31 @@
 import pandas as pd
 import os
-import requests
 import re
-from bs4 import BeautifulSoup
 import multiprocessing as mp
 import numpy as np
-import time
+import lyricsgenius
 
 agent = 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) \
         Gecko/20100101 Firefox/24.0'
 headers = {'User-Agent': agent}
+client_access_token = "LUl9sVGxJr623jLbPw79kFft3tdgwAu2ijT534W_2w6soOoR2H81SWT6xSIBo2J7"
+genius = lyricsgenius.Genius(client_access_token)
 
 if(os.path.exists("output.csv")):
     os.remove("output.csv")
 
-df = pd.read_csv("file.csv")
-
-def lyrics(artist, title):
-    time.sleep(1)
-    base = "https://azlyrics.com/"
-    artistURI = re.sub('[\W_]+', '', artist.lower())
-    titleURI = title.split('-', 1)[0].split('(', 1)[0]
-    titleURI = re.sub('[\W_]+', '', titleURI.lower())
-
-    url = base + 'lyrics/' + artistURI + '/' + titleURI + '.html'
-    req = requests.get(url, headers=headers)
-    soup = BeautifulSoup(req.content, "html.parser")
-    l = soup.find_all("div", attrs={"class": None, "id": None})
-    if not l:
-        return "Unable to find " + title + " by " + artist
-    else:
-        l = [x.getText() for x in l]
-        l = re.sub("\\n", " ", l)
-        return l
+df = pd.read_csv("../SpotifyAPI/ArtistAndSongs.csv")
 
 def grabLyrics(row):
-    print(row[1])
-    return lyrics(row[1],row[0])
+    song = genius.search_song(row[1], row[0])
+    return song.lyrics if song is not None else None
 
 def applyGrabLyrics(df):
     lyrics = df.apply(grabLyrics,axis=1)
+    print(type(lyrics))
     return lyrics
 
-n_proc = 4
+n_proc = mp.cpu_count() * 10
 p = mp.Pool(n_proc)
 split_dfs = np.array_split(df, n_proc)
 pool_res = p.map(applyGrabLyrics, split_dfs)
