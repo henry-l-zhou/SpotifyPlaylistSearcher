b2a61eb169522ccc7c41c59e1db7d052eb5dc87a
Brian Hu <bhurobin221@gmail.com>
2020-03-11 22:47:38 -0500

    running tests on full csv

--- lyric-searcher/lyricsearch.py
+++ lyric-searcher/lyricsearch.py
@@ -8,25 +8,17 @@ import pandas as pd
 #     	if "love" in row[2]:
 #     		print (row)
 #     		print ("\n")
-
-user_input = "been"
-
+user_input="love"
 df = pd.read_csv("../lyric-grabber/output.csv")
 searched_list = []
 for i in range(df.shape[0]):
-	count = 0;
-
-	words = df.iloc[i]["Lyrics"]
-
-	words = re.findall(r"[\w']+|[.,!?;]", words)
-
-	
-	for word in words:
-
-		if user_input in word:
-			count += 1
-
-	searched_list.append([df.iloc[i]["title"], df.iloc[i]["artist"], count])
+    count = 0;
+    words = df.iloc[i]["Lyrics"]
+    words = re.findall(r"[\w']+|[.,!?;]", words)
+    for word in words:
+        if user_input in word:
+            count += 1
+    searched_list.append([df.iloc[i][" Song Name"], df.iloc[i]["Artist"], count])
 
 searched_list.sort(key = lambda r:r[2], reverse = True)
 print (searched_list)
